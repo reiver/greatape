4b856a2ab5232b6161c6c8816cdcbd6781045a90
Xeronith <xeronith@yahoo.com>
2022-09-01 19:18:47 +0430

    feat(server): :sparkles: standardize error structure

--- greataped/server/http_error.go
+++ greataped/server/http_error.go
@@ -1,10 +1,24 @@
 package server
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"encoding/json"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func newError(code int, message string) *fiber.Error {
+	data, _ := json.Marshal(struct {
+		Type    string
+		Version int
+		Payload any
+	}{
+		Type:    "server_error",
+		Version: 1,
+		Payload: message,
+	})
+
 	return &fiber.Error{
 		Code:    code,
-		Message: message,
+		Message: string(data),
 	}
 }
