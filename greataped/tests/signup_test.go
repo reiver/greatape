46db7166e63725fe7648d8dfc2911013fd96fd39
Xeronith <xeronith@yahoo.com>
2022-09-29 17:00:25 +0330

    test(app): :white_check_mark: signup process

--- greataped/tests/signup_test.go
+++ greataped/tests/signup_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"testing"
@@ -15,6 +16,23 @@ func TestSignup(t *testing.T) {
 		"email":    fmt.Sprintf("%s@%s", id, DOMAIN),
 		"password": "123456",
 		"username": id,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.FailNow()
+	}
+
+	result := &struct{ Code string }{}
+	if err := json.NewDecoder(resp.Body).Decode(result); err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err = Post("/api/v1/verify", Payload{
+		"email": fmt.Sprintf("%s@%s", id, DOMAIN),
+		"code":  result.Code,
 	})
 	if err != nil {
 		t.Fatal(err)
