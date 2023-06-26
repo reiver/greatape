d83b595223bbe0937ea8f8ab57ab6a38adb7f421
Xeronith <xeronith@yahoo.com>
2023-06-26 11:25:56 +0330

    refactor(components): :art: improve code structure

--- components/constants/resources.en-US.go
+++ components/constants/resources.en-US.go
@@ -2,78 +2,77 @@ package constants
 
 import . "github.com/xeronith/diamante/contracts/localization"
 
-// noinspection GoUnusedGlobalVariable
-var Errors = Resource{
-	// SYSTEM_ERRORS
-	ERROR_MESSAGE_INITIALIZE:                                       "initialize",
-	ERROR_MESSAGE_NOT_IMPLEMENTED:                                  "not_implemented",
-	ERROR_MESSAGE_OPERATION_FAILED:                                 "operation_failed",
-	ERROR_MESSAGE_OPERATION_NOT_SUPPORTED:                          "operation_not_supported",
-	ERROR_MESSAGE_UNRESOLVED_DEPENDENCIES:                          "unresolved_dependencies",
-	ERROR_MESSAGE_SYSTEM_COMPONENT_NOT_FOUND:                       "system_component_not_found",
-	ERROR_MESSAGE_DOCUMENT_NOT_FOUND:                               "document_not_found",
-	ERROR_MESSAGE_SYSTEM_SCHEDULE_NOT_FOUND:                        "system_schedule_not_found",
-	ERROR_MESSAGE_IDENTITY_NOT_FOUND:                               "identity_not_found",
-	ERROR_MESSAGE_ACCESS_CONTROL_NOT_FOUND:                         "access_control_not_found",
-	ERROR_MESSAGE_REMOTE_ACTIVITY_NOT_FOUND:                        "remote_activity_not_found",
-	ERROR_MESSAGE_CATEGORY_TYPE_NOT_FOUND:                          "category_type_not_found",
-	ERROR_MESSAGE_CATEGORY_NOT_FOUND:                               "category_not_found",
-	ERROR_MESSAGE_USER_NOT_FOUND:                                   "user_not_found",
-	ERROR_MESSAGE_ACTIVITY_PUB_OBJECT_NOT_FOUND:                    "activity_pub_object_not_found",
-	ERROR_MESSAGE_ACTIVITY_PUB_ACTIVITY_NOT_FOUND:                  "activity_pub_activity_not_found",
-	ERROR_MESSAGE_ACTIVITY_PUB_PUBLIC_KEY_NOT_FOUND:                "activity_pub_public_key_not_found",
-	ERROR_MESSAGE_ACTIVITY_PUB_LINK_NOT_FOUND:                      "activity_pub_link_not_found",
-	ERROR_MESSAGE_ACTIVITY_PUB_MEDIA_NOT_FOUND:                     "activity_pub_media_not_found",
-	ERROR_MESSAGE_ACTIVITY_PUB_INCOMING_ACTIVITY_NOT_FOUND:         "activity_pub_incoming_activity_not_found",
-	ERROR_MESSAGE_ACTIVITY_PUB_OUTGOING_ACTIVITY_NOT_FOUND:         "activity_pub_outgoing_activity_not_found",
-	ERROR_MESSAGE_ACTIVITY_PUB_FOLLOWER_NOT_FOUND:                  "activity_pub_follower_not_found",
-	ERROR_MESSAGE_SPI_NOT_FOUND:                                    "spi_not_found",
-	ERROR_MESSAGE_UNKNOWN_DOCUMENT:                                 "unknown_document",
-	ERROR_MESSAGE_UNKNOWN_SYSTEM_SCHEDULE:                          "unknown_system_schedule",
-	ERROR_MESSAGE_UNKNOWN_IDENTITY:                                 "unknown_identity",
-	ERROR_MESSAGE_UNKNOWN_ACCESS_CONTROL:                           "unknown_access_control",
-	ERROR_MESSAGE_UNKNOWN_REMOTE_ACTIVITY:                          "unknown_remote_activity",
-	ERROR_MESSAGE_UNKNOWN_CATEGORY_TYPE:                            "unknown_category_type",
-	ERROR_MESSAGE_UNKNOWN_CATEGORY:                                 "unknown_category",
-	ERROR_MESSAGE_UNKNOWN_USER:                                     "unknown_user",
-	ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_OBJECT:                      "unknown_activity_pub_object",
-	ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_ACTIVITY:                    "unknown_activity_pub_activity",
-	ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_PUBLIC_KEY:                  "unknown_activity_pub_public_key",
-	ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_LINK:                        "unknown_activity_pub_link",
-	ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_MEDIA:                       "unknown_activity_pub_media",
-	ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_INCOMING_ACTIVITY:           "unknown_activity_pub_incoming_activity",
-	ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_OUTGOING_ACTIVITY:           "unknown_activity_pub_outgoing_activity",
-	ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_FOLLOWER:                    "unknown_activity_pub_follower",
-	ERROR_MESSAGE_UNKNOWN_SPI:                                      "unknown_spi",
-	ERROR_MESSAGE_INVALID_ID:                                       "invalid_id",
-	ERROR_MESSAGE_INVALID_PARAMETERS:                               "invalid_parameters",
-	ERROR_MESSAGE_INVALID_USERNAME_FOR_CHECK_USERNAME_AVAILABILITY: "invalid_username",
-	ERROR_MESSAGE_INVALID_USERNAME_FOR_SIGNUP:                      "invalid_username",
-	ERROR_MESSAGE_INVALID_EMAIL_FOR_SIGNUP:                         "invalid_email",
-	ERROR_MESSAGE_INVALID_PASSWORD_FOR_SIGNUP:                      "invalid_password",
-	ERROR_MESSAGE_INVALID_EMAIL_FOR_RESEND_VERIFICATION_CODE:       "invalid_email",
-	ERROR_MESSAGE_INVALID_EMAIL_FOR_VERIFY:                         "invalid_email",
-	ERROR_MESSAGE_INVALID_EMAIL_FOR_LOGIN:                          "invalid_email",
-	ERROR_MESSAGE_INVALID_PASSWORD_FOR_LOGIN:                       "invalid_password",
-	ERROR_MESSAGE_INVALID_CURRENT_PASSWORD_FOR_CHANGE_PASSWORD:     "invalid_current_password",
-	ERROR_MESSAGE_INVALID_NEW_PASSWORD_FOR_CHANGE_PASSWORD:         "invalid_new_password",
-	ERROR_MESSAGE_INVALID_RESOURCE_FOR_WEBFINGER:                   "invalid_resource",
-	// CUSTOM_ERRORS
-	ERROR_MESSAGE_DATA_INTEGRITY_VIOLATION:             "data_integrity_violation",
-	ERROR_MESSAGE_INVALID_STATE:                        "invalid_state",
-	ERROR_MESSAGE_USER_NOT_REGISTERED:                  "user_not_registered",
-	ERROR_MESSAGE_USERNAME_OR_EMAIL_ALREADY_REGISTERED: "username_or_email_already_registered",
-	ERROR_MESSAGE_ACCOUNT_NOT_VERIFIED:                 "account_not_verified",
-	ERROR_MESSAGE_ACCOUNT_BLOCKED:                      "account_blocked",
-	ERROR_MESSAGE_INVALID_TOKEN:                        "invalid_token",
-	ERROR_MESSAGE_INVALID_CONFIRMATION_CODE:            "invalid_confirmation_code",
-	ERROR_MESSAGE_PERMISSION_DENIED:                    "permission_denied",
-	ERROR_MESSAGE_INVALID_PERSON_KIND:                  "invalid_person_kind",
-	ERROR_MESSAGE_INVALID_CREDENTIALS:                  "invalid_credentials",
-}
+var Errors = Resource{}
 
 func init() {
-	// CUSTOM_ERRORS
+	// Default Error Messages
+	Errors[ERROR_MESSAGE_INITIALIZE] = "initialize"
+	Errors[ERROR_MESSAGE_NOT_IMPLEMENTED] = "not_implemented"
+	Errors[ERROR_MESSAGE_OPERATION_FAILED] = "operation_failed"
+	Errors[ERROR_MESSAGE_OPERATION_NOT_SUPPORTED] = "operation_not_supported"
+	Errors[ERROR_MESSAGE_OPERATION_RUNNER_NOT_AVAILABLE] = "operation_runner_not_available"
+	Errors[ERROR_MESSAGE_OPERATION_PAYLOAD_NOT_SUPPORTED] = "operation_payload_not_supported"
+	Errors[ERROR_MESSAGE_UNRESOLVED_DEPENDENCIES] = "unresolved_dependencies"
+	Errors[ERROR_MESSAGE_SYSTEM_COMPONENT_NOT_FOUND] = "system_component_not_found"
+	Errors[ERROR_MESSAGE_DOCUMENT_NOT_FOUND] = "document_not_found"
+	Errors[ERROR_MESSAGE_SYSTEM_SCHEDULE_NOT_FOUND] = "system_schedule_not_found"
+	Errors[ERROR_MESSAGE_IDENTITY_NOT_FOUND] = "identity_not_found"
+	Errors[ERROR_MESSAGE_ACCESS_CONTROL_NOT_FOUND] = "access_control_not_found"
+	Errors[ERROR_MESSAGE_REMOTE_ACTIVITY_NOT_FOUND] = "remote_activity_not_found"
+	Errors[ERROR_MESSAGE_CATEGORY_TYPE_NOT_FOUND] = "category_type_not_found"
+	Errors[ERROR_MESSAGE_CATEGORY_NOT_FOUND] = "category_not_found"
+	Errors[ERROR_MESSAGE_USER_NOT_FOUND] = "user_not_found"
+	Errors[ERROR_MESSAGE_ACTIVITY_PUB_OBJECT_NOT_FOUND] = "activity_pub_object_not_found"
+	Errors[ERROR_MESSAGE_ACTIVITY_PUB_ACTIVITY_NOT_FOUND] = "activity_pub_activity_not_found"
+	Errors[ERROR_MESSAGE_ACTIVITY_PUB_PUBLIC_KEY_NOT_FOUND] = "activity_pub_public_key_not_found"
+	Errors[ERROR_MESSAGE_ACTIVITY_PUB_LINK_NOT_FOUND] = "activity_pub_link_not_found"
+	Errors[ERROR_MESSAGE_ACTIVITY_PUB_MEDIA_NOT_FOUND] = "activity_pub_media_not_found"
+	Errors[ERROR_MESSAGE_ACTIVITY_PUB_INCOMING_ACTIVITY_NOT_FOUND] = "activity_pub_incoming_activity_not_found"
+	Errors[ERROR_MESSAGE_ACTIVITY_PUB_OUTGOING_ACTIVITY_NOT_FOUND] = "activity_pub_outgoing_activity_not_found"
+	Errors[ERROR_MESSAGE_ACTIVITY_PUB_FOLLOWER_NOT_FOUND] = "activity_pub_follower_not_found"
+	Errors[ERROR_MESSAGE_SPI_NOT_FOUND] = "spi_not_found"
+	Errors[ERROR_MESSAGE_UNKNOWN_DOCUMENT] = "unknown_document"
+	Errors[ERROR_MESSAGE_UNKNOWN_SYSTEM_SCHEDULE] = "unknown_system_schedule"
+	Errors[ERROR_MESSAGE_UNKNOWN_IDENTITY] = "unknown_identity"
+	Errors[ERROR_MESSAGE_UNKNOWN_ACCESS_CONTROL] = "unknown_access_control"
+	Errors[ERROR_MESSAGE_UNKNOWN_REMOTE_ACTIVITY] = "unknown_remote_activity"
+	Errors[ERROR_MESSAGE_UNKNOWN_CATEGORY_TYPE] = "unknown_category_type"
+	Errors[ERROR_MESSAGE_UNKNOWN_CATEGORY] = "unknown_category"
+	Errors[ERROR_MESSAGE_UNKNOWN_USER] = "unknown_user"
+	Errors[ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_OBJECT] = "unknown_activity_pub_object"
+	Errors[ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_ACTIVITY] = "unknown_activity_pub_activity"
+	Errors[ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_PUBLIC_KEY] = "unknown_activity_pub_public_key"
+	Errors[ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_LINK] = "unknown_activity_pub_link"
+	Errors[ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_MEDIA] = "unknown_activity_pub_media"
+	Errors[ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_INCOMING_ACTIVITY] = "unknown_activity_pub_incoming_activity"
+	Errors[ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_OUTGOING_ACTIVITY] = "unknown_activity_pub_outgoing_activity"
+	Errors[ERROR_MESSAGE_UNKNOWN_ACTIVITY_PUB_FOLLOWER] = "unknown_activity_pub_follower"
+	Errors[ERROR_MESSAGE_UNKNOWN_SPI] = "unknown_spi"
+	Errors[ERROR_MESSAGE_INVALID_ID] = "invalid_id"
+	Errors[ERROR_MESSAGE_INVALID_PARAMETERS] = "invalid_parameters"
+	Errors[ERROR_MESSAGE_INVALID_USERNAME_FOR_CHECK_USERNAME_AVAILABILITY] = "invalid_username"
+	Errors[ERROR_MESSAGE_INVALID_USERNAME_FOR_SIGNUP] = "invalid_username"
+	Errors[ERROR_MESSAGE_INVALID_EMAIL_FOR_SIGNUP] = "invalid_email"
+	Errors[ERROR_MESSAGE_INVALID_PASSWORD_FOR_SIGNUP] = "invalid_password"
+	Errors[ERROR_MESSAGE_INVALID_EMAIL_FOR_RESEND_VERIFICATION_CODE] = "invalid_email"
+	Errors[ERROR_MESSAGE_INVALID_EMAIL_FOR_VERIFY] = "invalid_email"
+	Errors[ERROR_MESSAGE_INVALID_EMAIL_FOR_LOGIN] = "invalid_email"
+	Errors[ERROR_MESSAGE_INVALID_PASSWORD_FOR_LOGIN] = "invalid_password"
+	Errors[ERROR_MESSAGE_INVALID_CURRENT_PASSWORD_FOR_CHANGE_PASSWORD] = "invalid_current_password"
+	Errors[ERROR_MESSAGE_INVALID_NEW_PASSWORD_FOR_CHANGE_PASSWORD] = "invalid_new_password"
+	Errors[ERROR_MESSAGE_INVALID_RESOURCE_FOR_WEBFINGER] = "invalid_resource"
+	Errors[ERROR_MESSAGE_DATA_INTEGRITY_VIOLATION] = "data_integrity_violation"
+	Errors[ERROR_MESSAGE_INVALID_STATE] = "invalid_state"
+	Errors[ERROR_MESSAGE_USER_NOT_REGISTERED] = "user_not_registered"
+	Errors[ERROR_MESSAGE_USERNAME_OR_EMAIL_ALREADY_REGISTERED] = "username_or_email_already_registered"
+	Errors[ERROR_MESSAGE_ACCOUNT_NOT_VERIFIED] = "account_not_verified"
+	Errors[ERROR_MESSAGE_ACCOUNT_BLOCKED] = "account_blocked"
+	Errors[ERROR_MESSAGE_INVALID_TOKEN] = "invalid_token"
+	Errors[ERROR_MESSAGE_INVALID_CONFIRMATION_CODE] = "invalid_confirmation_code"
+	Errors[ERROR_MESSAGE_PERMISSION_DENIED] = "permission_denied"
+	Errors[ERROR_MESSAGE_INVALID_PERSON_KIND] = "invalid_person_kind"
+	Errors[ERROR_MESSAGE_INVALID_CREDENTIALS] = "invalid_credentials"
+	// Overrides
 	Errors[ERROR_MESSAGE_DATA_INTEGRITY_VIOLATION] = "data_integrity_violation"
 	Errors[ERROR_MESSAGE_INVALID_PASSWORD_FOR_SIGNUP] = "Your password should be at least 7 characters long including uppercase and lowercase letters, numbers and special characters."
 	Errors[ERROR_MESSAGE_INVALID_PASSWORD_FOR_LOGIN] = "Your password should be at least 7 characters long including uppercase and lowercase letters, numbers and special characters."
--- components/contracts/opcodes.go
+++ components/contracts/opcodes.go
@@ -1,7 +1,5 @@
 package contracts
 
-import . "github.com/xeronith/diamante/contracts/server"
-
 // noinspection GoSnakeCaseUsage
 const (
 	//SystemCallOperation
@@ -100,53 +98,3 @@ const (
 	GET_INBOX_REQUEST = 0x4B8B9285
 	GET_INBOX_RESULT  = 0x65835436
 )
-
-var OPCODES = Opcodes{
-	0x00000000: "N/A",
-	0x0541BD72: "ECHO",
-	0xAB2FF7D4: "Echo",
-	0xC89C311F: "GET_SERVER_CONFIGURATION",
-	0xA1A50921: "GetServerConfiguration",
-	0xA9501A55: "CHECK_USERNAME_AVAILABILITY",
-	0x067190FF: "CheckUsernameAvailability",
-	0x48DB23BF: "SIGNUP",
-	0x83D062B4: "Signup",
-	0x03373782: "RESEND_VERIFICATION_CODE",
-	0x6B101891: "ResendVerificationCode",
-	0x8B78F7F6: "VERIFY",
-	0x2C8A8A49: "Verify",
-	0xF480F151: "LOGIN",
-	0xBE819605: "Login",
-	0xEAB16E71: "GET_PROFILE_BY_USER",
-	0x8EECDE97: "GetProfileByUser",
-	0xC25AB0BA: "UPDATE_PROFILE_BY_USER",
-	0x678A8BAF: "UpdateProfileByUser",
-	0x926A5565: "CHANGE_PASSWORD",
-	0x521E68DF: "ChangePassword",
-	0xF4030036: "RESET_PASSWORD",
-	0x3D70F105: "ResetPassword",
-	0x447AFA34: "LOGOUT",
-	0x9412D17F: "Logout",
-	0x01FD357C: "WEBFINGER",
-	0xCC81EC52: "Webfinger",
-	0x157C555E: "GET_PACKAGES",
-	0x4CBD814E: "GetPackages",
-	0x5C4AC410: "GET_ACTOR",
-	0x136B82A8: "GetActor",
-	0xD30C2420: "FOLLOW_ACTOR",
-	0x30154D74: "FollowActor",
-	0x59EA7612: "AUTHORIZE_INTERACTION",
-	0xB38E936F: "AuthorizeInteraction",
-	0x3F20FD65: "GET_FOLLOWERS",
-	0x7F3E2EB5: "GetFollowers",
-	0xF9841DB9: "GET_FOLLOWING",
-	0xD707408F: "GetFollowing",
-	0x9E489553: "POST_TO_OUTBOX",
-	0xC6C56614: "PostToOutbox",
-	0x527B6997: "GET_OUTBOX",
-	0xF94E37A0: "GetOutbox",
-	0x7400C331: "POST_TO_INBOX",
-	0x5BD43D90: "PostToInbox",
-	0x4B8B9285: "GET_INBOX",
-	0x65835436: "GetInbox",
-}
